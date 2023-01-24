7917cfa9933ac0f3dd1cdc697a6fd5097834dd4b
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-01-25 05:54:04 +0900

    不要なファイルを削除 / プロジェクトを作成

---
+++ Source/Main.cs
@@ -0,0 +1,11 @@
+﻿using Produire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transitions.rdr
+{
+
+}
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using Produire;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("Transitions.rdr")]
+[assembly: AssemblyDescription("Template")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Author")]
+[assembly: AssemblyProduct("Transitions.rdr")]
+[assembly: AssemblyCopyright("Copyright (C) 2022 Author")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: PluginName("Transitions.rdr")]
+[assembly: PluginDescription("Template")]
+[assembly: PluginVersion("1.0")]
+[assembly: PluginAcceptVersion("1.0")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("95e3ca8f-5e12-411b-8320-4712a0c66bf1")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
