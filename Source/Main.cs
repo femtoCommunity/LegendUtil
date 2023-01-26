0c73079c60ce2a70af809470de3d5a0b0a108a2d
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-01-27 00:46:06 +0900

    不要なファイルを削除 / ソースを追加

---
+++ Source/Main.cs
@@ -0,0 +1,49 @@
+﻿using Produire;
+using System;
+using static ClipCursor.ClipCursor_Main;
+
+namespace LockCursorInWindowPlugin
+{
+	public class カーソルロッカー : IProduireStaticClass
+	{
+		bool IsLocked = false;
+
+		// 手順
+		[自分("で")]
+		public void ロックする([として] int ウィンドウハンドル)
+		{
+			IntPtr i = new IntPtr(ウィンドウハンドル);
+			try
+			{
+				LockCursor(i);
+				IsLocked = true;
+			}
+			catch (Exception ex)
+			{
+				throw new ProduireException(ex);
+			}
+		}
+
+		[自分("で")]
+		public void ロック解除する()
+		{
+			try
+			{
+				Locked = false;
+				IsLocked = false;
+			}
+			catch (Exception ex)
+			{
+				throw new ProduireException(ex);
+			}
+		}
+
+		public bool ロック中
+		{
+			get
+			{
+				return IsLocked;
+			}
+		}
+	}
+}
