b306a864eb1890d37fea555feb9a760444b5720d
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-03-11 05:44:18 +0900

    名称変更

--- Source/ResolutionChangerPlugin/Properties/AssemblyInfo.cs
+++ Source/ResolutionChangerPlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ResolutionChangerPlugin")]
+[assembly: AssemblyTitle("LegendUtil Display Device Helper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Milkeyyy")]
-[assembly: AssemblyProduct("ResolutionChangerPlugin")]
-[assembly: AssemblyCopyright("Copyright © 2022 Milkeyyy")]
+[assembly: AssemblyProduct("LegendUtil.DisplayDeviceHelper")]
+[assembly: AssemblyCopyright("Copyright © 2023 Milkeyyy")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
