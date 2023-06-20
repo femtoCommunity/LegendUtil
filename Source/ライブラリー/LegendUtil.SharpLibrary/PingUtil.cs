8ba3ea7d5e111cf415a02d829b73c1cb9f2115cb
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-06-20 14:46:12 +0900

    SharpLibraryのコミット忘れ (フォルダー選択ダイアログが消えていたので修正) / WindowsAPICodePackを別バージョンへ更新

---
+++ Source/ライブラリー/LegendUtil.SharpLibrary/PingUtil.cs
@@ -0,0 +1,35 @@
+﻿using Produire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Diagnostics.SymbolStore;
+using NetInfo = System.Net.NetworkInformation;
+
+namespace LegendUtil.SharpLibrary
+{
+	namespace PingUtil
+	{
+		public class Ping : IProduireStaticClass
+		{
+			NetInfo.Ping pingSender = new NetInfo.Ping();
+			NetInfo.PingOptions options = new NetInfo.PingOptions();
+
+			[自分で]
+			public long レイテンシー取得する([から] string アドレス)
+			{
+				NetInfo.PingReply reply = pingSender.Send(アドレス);
+				if (reply.Status == NetInfo.IPStatus.Success)
+				{
+					return reply.RoundtripTime;
+				}
+				else
+				{
+					return -1;
+				}
+			}
+		}
+	}
+}
